a9baa7157fc42babc24d73add9b4ea21311b2428
Bruno Ramalho <bfmramalho@gmail.com>
2019-12-29 15:26:03 +0100

    produce message

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+)
 
 func main() {
-	fmt.Println("It works!")
+	fmt.Println("Sending message...")
+
+	p := GetProducer()
+	topic := "myTopic"
+	for _, word := range []string{"Message One", "Message Tow"} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+
+	fmt.Println("Getting message...")
+	// ...
 }
